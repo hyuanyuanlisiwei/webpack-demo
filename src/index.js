2426ac0af2b7dd46ce5d672dddd31217c9d244af
huyuanyuan006 <huyuanyuan006@ke.com>
2019-04-11 15:51:28 +0800

    回退处理

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import _ from 'lodash'
-import './style.css';
-// import Icon from './icon.png';
 function component() {
   var element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  // 将图像添加到我们现有的 div。
-  // let myIcon = new Image();
-  // myIcon.src = Icon;
-  // element.appendChild(myIcon);
+
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,46 +8,6 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader','css-loader']
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          query: {
-            plugins: ['lodash'],
-            presets: ['es2015']
-          }
-        }]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              }
-            },
-          },
-        ]
-      },
-      {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-             'file-loader'
-           ]
-       }
-    ]
-  },
   plugins: [
     new BundleAnalyzerPlugin(),
     new LodashModuleReplacementPlugin()
